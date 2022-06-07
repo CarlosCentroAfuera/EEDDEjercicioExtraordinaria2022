42857943f9969413583bf96690a07af54eebfe04
CarlosCentroAfuera <72136781+CarlosCentroAfuera@users.noreply.github.com>
2022-06-07 09:13:30 +0200

    Update RecuTest.java

--- src/test/java/RecuTest.java
+++ src/test/java/RecuTest.java
@@ -12,7 +12,7 @@ public class RecuTest {
     // Para obtener una persona:
     //      examen.personas.getResults()[0]
 
-    // TODO: (2,5 puntos) Piensa en que test son necesarios para probar la función "esIrlandes" y realizalos.
+    // TODO: (3 puntos) Piensa en que test son necesarios para probar la función "esIrlandes" y realizalos.
     //  Si la persona es null deberá devolver la excepción PersonaNulaException
     //  Si la "nat" no es "IE" pero vive en "Ireland", deberá devolver ErrorEnNacionalidadException
     //  Si la "nat" es "IE" pero no vive en "Ireland", deberá devolver ErrorEnNacionalidadException
@@ -20,7 +20,7 @@ public class RecuTest {
     //  esIrlandes deberá devolver "true" si es irlandés. En caso contrario, "false"
     @Test
     public void testEsIrlandes_XXX() {
-        // TODO 2p
+        // TODO 3p
 
     }
 
